9d1f533133c6bafdda813a8231ad3f5ae83da00d
Arthur Edelstein <arthuredelstein@gmail.com>
2020-03-19 07:59:00 -0700

    allow 1-up, 2-up and thumbnails

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,6 @@
 document.addEventListener("keypress", event => {
-  console.log("Hello, Julian and Alec!");
+  //console.log("Hello, Julian and Alec!");
+  //console.log(event);
   if (event.key === "f") {
     if (document.fullscreenElement === null) {
       let brContainer = document.querySelector(".BRcontainer");
@@ -9,6 +10,12 @@ document.addEventListener("keypress", event => {
     } else {
       document.exitFullscreen();
     }
+  } else if (event.key === "1") {
+    window.eval("br.switchMode(br.constMode1up);");
+  } else if (event.key === "2") {
+    window.eval("br.switchMode(br.constMode2up);");
+  } else if (event.key === "t") {
+    window.eval("br.switchMode(br.constModeThumb);");
   }
 });
 
